d9503a4702e045a5de3df3664b98cb080d067b29
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-02-19 12:17:42 +0530

    added mysql config

---
+++ src/test/java/com/sprint_security/Spring_Security_2/SpringSecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint_security.Spring_Security_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
